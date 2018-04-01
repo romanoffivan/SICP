d833322e972b447c5f8662a39c51e1d1f7ad2be1
romanoffivan <iw.romanof@yandex.ru>
2018-04-01 18:22:49 +0300

    fix

--- chapter01/1.3.rkt
+++ chapter01/1.3.rkt
@@ -1,9 +1,12 @@
-#lang racket
+#lang racket/base
+
+(require rackunit)
 
 (define (square x) (* x x))
 
-(define (sum-of-square x y z)
+(define (solution x y z)
   (cond ((and (< x y) (< x z)) (+ (square y) (square z)))
         ((and (< y x) (< y z)) (+ (square x) (square z)))
         (else (+ (square x) (square y)))))
-        
+
+(check-equal? (solution 1 2 3) 13 (printf "test 1.3 passed"))
